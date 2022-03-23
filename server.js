c02036799a117394d4d423fe1b3aefc8e75dfa70
Wemeson Araujo <wemesonaraujo@Wemesons-iMac.local>
2022-03-23 13:57:51 -0400

    add .gitignore

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+
+const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+
+app.set('views', path.resolve(__dirname, 'src', 'views')); // I can pass the relative path too like: './scr/views'
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+
+
+app.listen(PORT, ()=> {console.log(`Acess: http://localhost:${PORT}`);});
---
+++ src/controllers/contatoController.js
@@ -0,0 +1,7 @@
+exports.paginaInicial = (req, res) => {
+    res.send(`Obrigado por entrar em contato`);
+};
+
+exports.trataPost = (req, res) => {
+    res.send('Eu sou sua rota de poste');
+};

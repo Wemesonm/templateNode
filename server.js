9fd1906e74fb4d87c0e809912e74091a20526227
Wemeson Araujo <wemesonaraujo@Wemesons-iMac.local>
2022-03-23 14:12:01 -0400

    removed node_modules

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.static(path.resolve(__dirname, 'public'))); // I can pass the relative path too like: './public'
+
 app.set('views', path.resolve(__dirname, 'src', 'views')); // I can pass the relative path too like: './scr/views'
 app.set('view engine', 'ejs');
 
